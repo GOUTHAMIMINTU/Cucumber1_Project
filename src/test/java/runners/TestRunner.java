ec026d38a4e832f78f74b10e7cc76631d97dac81
Gouthami <boosa.gouthami@capgemini.com>
2023-02-25 14:26:24 +0530

    modified files are added

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,13 +8,13 @@ import org.testng.annotations.DataProvider;
 
 
 @CucumberOptions(features = "src/test/resources/features" ,glue="steps",publish = true,
-        plugin = { "pretty", "html:target/cucumber-reports.html","json:target/cucumber-reports.json" },
+        plugin = { "pretty", "html:target/cucumber-reports.html","json:target/cucumber-reports.json"},
         monochrome = true)//, tags = "@smoke"
 
 public class TestRunner extends AbstractTestNGCucumberTests {
     @DataProvider(parallel = true)
     public Object[][]scenarios(){
-        return super.scenarios();
+        return super.scenarios();//"json:target/cucumber-reports.json"
 
     }
 
